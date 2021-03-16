239f40faf4ca25cac1a9b180d5ebcc5cf4587c13
Vitaliy <prokinvs@gmail.com>
2021-03-16 19:22:49 +0300

    Обработка отключения переадресации

--- Server/server.js
+++ Server/server.js
@@ -3,8 +3,9 @@
 import express from 'express';
 import { post } from 'axios';
 import { inspect } from 'util';
-import { parse } from 'url';
+import { url } from 'url';
 import low from 'lowdb';
+import moment from 'moment';
 import FileSync from 'lowdb/adapters/FileSync';
 import { info, error } from '../logger/logger';
 
@@ -13,10 +14,8 @@ const app = express();
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-app.use((req, res, next) => {
-    info(req.url);
-    next();
-});
+db.defaults({ forward: [], mail: [], count: 0 })
+    .write();
 
 async function setInfoToDB(type, data) {
     info(type, data);
@@ -25,8 +24,53 @@ async function setInfoToDB(type, data) {
         .write();
 }
 
-db.defaults({ forward: [], mail: [], count: 0 })
-    .write();
+// Удаление информации по переадресации из БД
+async function deleteIDInDB(id) {
+    try {
+        info(id);
+        const resultDelete = await db.get('forward')
+            .remove({ id })
+            .write();
+        return resultDelete;
+    } catch (e) {
+        error(`Ошибка удаления из базы deleteIDInDB ${e}`);
+    }
+}
+
+// Выгрузка из базы данных всех правил, по которым ранее была переадресация, статусы которых надо вернуть обратно
+async function searchInDB(exten, type, number) {
+    try {
+        let id;
+        const forward = await db.get('forward')
+            .value();
+        /* eslint-disable-next-line */
+        for (const key of forward) {
+            if (key.exten == exten && key.type == type && key.number == number) {
+                info(`Найден ID ${key.id}`);
+                id = key.id;
+            } else {
+                info(`По запросу ничего не найдено ${exten} ${type} ${number}`);
+            }
+        }
+        return id;
+    } catch (e) {
+        error(`Ошибка поиска в базе searchInDB ${e}`);
+    }
+}
+
+async function sendModifyStatus(urlString) {
+    try {
+        const result = await post(`http://172.16.0.2:3000${urlString}`);
+        return result.status;
+    } catch (e) {
+        error(`Ошибка изменения статуса sendModifyStatus ${e}`);
+    }
+}
+
+app.use((req, res, next) => {
+    info(req.url);
+    next();
+});
 
 app.post('/queue*', async(req, res) => {
     try {
@@ -52,29 +96,51 @@ app.post('/mail*', (req, res) => {
 
 app.post('/forward*', async(req, res) => {
     try {
-        const queryData = parse(req.url, true).query;
+        const today = moment().format('DD.MM.YYYY');
+        const {
+            exten,
+            type,
+            number,
+            dateFrom,
+            dateTo,
+            status,
+        } = url.parse(req.url, true).query;
+
         const data = {
             id: new Date().getTime() / 1000,
-            exten: queryData.exten,
-            type: queryData.type,
-            number: queryData.number,
-            dateFrom: queryData.dateFrom,
-            dateTo: queryData.dateTo,
-            status: true,
+            exten: exten,
+            type: type,
+            number: number,
+            dateFrom: dateFrom,
+            dateTo: dateTo,
+            status: status,
         };
-        const result = await post(`http://172.16.0.2:3000${req.url}&status=true`);
-        if (result.status == 200) {
-            info(`Статус переадресации ${inspect(result)}`);
+
+        if (dateFrom == today || status == 'true') {
+            const resultSendModifyStatus = await sendModifyStatus(req.url);
+            if (resultSendModifyStatus == 200) {
+                await setInfoToDB('forward', data);
+                res.status(200).end();
+            } else {
+                res.status(503).end();
+            }
+        } else if (status == 'false') {
+            const resultSendModifyStatus = await sendModifyStatus(req.url);
+            if (resultSendModifyStatus == 200) {
+                const resultSearch = await searchInDB(exten, type, number);
+                const resultDeleteInDB = await deleteIDInDB(resultSearch);
+                info(`Получен результат удаления ${resultDeleteInDB}`);
+                res.status(200).end();
+            } else {
+                res.status(503).end();
+            }
+        } else {
             const resultPushDB = await setInfoToDB('forward', data);
             info(`Результат занесения в БД ${inspect(resultPushDB)}`);
             res.status(200).end();
-        } else {
-            error(`Проблемы с изменение статуса переадресации ${result}`);
-            res.status(503).end();
         }
     } catch (e) {
         error(`Проблемы с изменение статуса переадресации ${inspect(e)}`);
-        res.status(503).end();
     }
 });
 
